95f5546fdf5482b3642f8d3d7db77495e563e9fa
eidolonic <brothers.dave@gmail.com>
2019-09-01 20:18:07 -0400

    Remove unused dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,38 @@
 var babel = require("gulp-babel");
-//var buffer = require("vinyl-buffer");
 var del = require("del");
 var gulp = require("gulp");
 var rename = require("gulp-rename");
 var server = require("gulp-webserver");
-//var source = require("vinyl-source-stream");
 var sourcemaps = require("gulp-sourcemaps");
 var terser = require("gulp-terser");
 
-// function buildScript(filename) {
-//   var browserified = browserify({
-//     entries: `./dist/scripts/${filename}.js`,
-//     debug: true
-//   });
-
-//   return browserified
-//     .bundle()
-//     .pipe(source(`${filename}.min.js`))
-//     .pipe(buffer())
-//     .pipe(sourcemaps.init({ loadMaps: true }))
-//     .pipe(terser({
-//       ecma: 5
-//     }))
-//     .on("error", console.error)
-//     .pipe(sourcemaps.write("./"))
-//     .pipe(gulp.dest("./dist/scripts/"));
-// }
-
-// function buildScript() {
-//   return gulp
-//     .src("./src/scripts/*.js")
-//     .pipe(gulp.dest("./dist/scripts/"))
-//     .pipe(
-//       babel({
-//         presets: [
-//           [
-//             "@babel/preset-env",
-//             {
-//               useBuiltIns: "entry"
-//             }
-//           ]
-//         ]
-//       })
-//     )
-//     .pipe(terser())
-//     .pipe(rename(path => (path.basename += ".min")))
-//     .pipe(gulp.dest("./dist/scripts"));
-// }
+gulp.task("build:scripts", () => {
+  return gulp
+    .src("./src/scripts/*.js")
+    .pipe(gulp.dest("./dist/scripts/"))
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: "> 0.25%, not dead"
+            }
+          ]
+        ]
+      })
+    )
+    .pipe(terser(
+      {
+        keep_fnames: false,
+        mangle: true
+      }
+    ))
+    .pipe(rename(path => (path.basename += ".min")))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("./dist/scripts"));
+});
 
 gulp.task("clean:all", function() {
   return del("dist/*");
@@ -67,29 +53,6 @@ gulp.task("serve:host", function() {
 
 gulp.task("serve:watch", function() {
   return gulp.watch("./src/**/*", gulp.parallel("build"));
-});
-
-gulp.task("build:scripts", () => {
-  return gulp
-    .src("./src/scripts/*.js")
-    .pipe(gulp.dest("./dist/scripts/"))
-    .pipe(sourcemaps.init())
-    .pipe(
-      babel({
-        presets: [
-          [
-            "@babel/preset-env",
-            {
-              useBuiltIns: "entry"
-            }
-          ]
-        ]
-      })
-    )
-    .pipe(terser())
-    .pipe(rename(path => (path.basename += ".min")))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("./dist/scripts"));
 });
 
 gulp.task(
